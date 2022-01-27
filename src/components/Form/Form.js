fc656ee7c5067c4b1194b1b184df11e2eed92d09
Obada Qawwas <obadaqawwas@teknasyon.com>
2022-01-27 18:32:04 +0300

    [refactor] improve code design.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,32 +1,37 @@
-import React from "react";
-import './Form.css';
+import React, { useState } from "react";
+import "./Form.css";
 
+const Form = ({ onAddTodo, onSelectChange }) => {
+    const [enteredTodoValue, setEnteredTodoValue] = useState("");
 
-const Form = (props) => {
-  const todoHandler = (e) => {
-    props.setEnteredTodoValue(e.target.value);
-  }
-  const submitTodoHandler = (e) => {
-    e.preventDefault();
-    props.setTodos([...props.todos, {text: props.enteredTodoValue, completed: false, id: Math.random() * 1000 }])
-    props.setEnteredTodoValue('');
-  }
-  const statusHandler = (e) => {
-    e.preventDefault();
-    props.setStatus(e.target.value)
-  }
-  return (
-    <form >
-      <input type="text" value={props.enteredTodoValue} onChange={todoHandler} className="todo-input" />
-      <button type="submit" onClick={submitTodoHandler} className="todo-button">Add</button>
-      <select className="select" onChange={statusHandler}>
-        <option value="all">All</option>
-        <option value="completed">Completed</option>
-        <option value="uncompleted">Uncompleted</option>
-      </select>
-    </form>
-  );
-}
+    const todoHandler = (e) => {
+        setEnteredTodoValue(e.target.value);
+    };
 
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        onAddTodo({ text: enteredTodoValue, completed: false, id: Math.random() * 1000 });
+        setEnteredTodoValue("");
+    };
+
+    const statusHandler = (e) => {
+        e.preventDefault();
+        onSelectChange(e.target.value);
+    };
+
+    return (
+        <form onSubmit={submitTodoHandler}>
+            <input type="text" value={enteredTodoValue} onChange={todoHandler} className="todo-input" />
+            <button type="submit" className="todo-button">
+                Add
+            </button>
+            <select className="select" onChange={statusHandler}>
+                <option value="all">All</option>
+                <option value="completed">Completed</option>
+                <option value="uncompleted">Uncompleted</option>
+            </select>
+        </form>
+    );
+};
 
 export default Form;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,18 +2,29 @@ import React from "react";
 import Todo from "../Todo/Todo";
 import "./TodoList.css";
 
-const TodoList = ({setTodos,todos,filteredTodos}) => {
-
-  return (
+const TodoList = ({ onUpdateTodo, onRemoveTodo, todos, status }) => (
     <div className="todo-container">
-      <ul className="todo-list">
-        {filteredTodos.map((todo) => (
-          <Todo setTodos={setTodos} todos={todos} todo={todo} id={todo.id} key={todo.id} />
-        ))}
-      </ul>
-    </div>
-  )
-}
+        <ul className="todo-list">
+            {todos.map((todo) => {
+                const isAll = status === "all";
+                const isCompleted = status === "completed";
+                const newPorps = {
+                    onUpdateTodo: onUpdateTodo,
+                    onRemoveTodo: onRemoveTodo,
+                    todo: todo,
+                    key: todo.id
+                };
 
+                if (isAll) {
+                    return <Todo {...newPorps} />;
+                } else if (isCompleted === todo.completed) {
+                    return <Todo {...newPorps} />;
+                }
+
+                return null;
+            })}
+        </ul>
+    </div>
+);
 
 export default TodoList;
